85afa7c076d56290b33b27ec0946c7c41220a11e
Ritik Harchani <harchani.ritik@gmail.com>
2019-05-12 12:03:19 +0530

    All four NewActivites Added

--- app/src/main/java/com/example/android/languagelearner/MainActivity.java
+++ app/src/main/java/com/example/android/languagelearner/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.languagelearner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void Numbers(View view)
+    {
+        Intent i= new Intent(MainActivity.this,Numbers.class);
+        startActivity(i);
+    }
+    public void Family(View view)
+    {
+        Intent i= new Intent(MainActivity.this,FamilyMembers.class);
+        startActivity(i);
+    }
+    public void Colours(View view)
+    {
+        Intent i= new Intent(MainActivity.this,Colours.class);
+        startActivity(i);
+    }
+    public void Phrases(View view)
+    {
+        Intent i= new Intent(MainActivity.this,Phrases.class);
+        startActivity(i);
     }
 }
