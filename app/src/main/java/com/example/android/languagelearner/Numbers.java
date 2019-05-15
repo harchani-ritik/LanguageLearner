23b640c90ebcdd2df61e24fb8f7409e77de79040
Ritik Harchani <harchani.ritik@gmail.com>
2019-05-15 22:57:40 +0530

    Use ListView + ArrayAdapter

--- app/src/main/java/com/example/android/languagelearner/Numbers.java
+++ app/src/main/java/com/example/android/languagelearner/Numbers.java
@@ -2,7 +2,9 @@ package com.example.android.languagelearner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -27,12 +29,8 @@ public class Numbers extends AppCompatActivity {
         words.add("nine");
         words.add("ten");
 
-        LinearLayout numbersView = (LinearLayout) findViewById(R.id.numbersView);
-
-        for(int index=0;index<10;index++) {
-            TextView wordView = new TextView(this);
-            wordView.setText(words.get(index));
-            numbersView.addView(wordView);
-        }
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(itemsAdapter);
     }
 }
