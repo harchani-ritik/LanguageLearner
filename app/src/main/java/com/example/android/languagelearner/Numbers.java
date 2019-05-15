dcbad1ffe62388ef8a9a57948782474d0b56ba7e
Ritik Harchani <harchani.ritik@gmail.com>
2019-05-15 11:12:29 +0530

    Added ArrayList in Numbers

--- app/src/main/java/com/example/android/languagelearner/Numbers.java
+++ app/src/main/java/com/example/android/languagelearner/Numbers.java
@@ -2,6 +2,10 @@ package com.example.android.languagelearner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class Numbers extends AppCompatActivity {
 
@@ -9,5 +13,26 @@ public class Numbers extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        ArrayList<String> words = new ArrayList<String>();
+
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+        LinearLayout numbersView = (LinearLayout) findViewById(R.id.numbersView);
+
+        for(int index=0;index<10;index++) {
+            TextView wordView = new TextView(this);
+            wordView.setText(words.get(index));
+            numbersView.addView(wordView);
+        }
     }
 }
