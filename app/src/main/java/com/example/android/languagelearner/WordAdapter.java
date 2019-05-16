39c3d92b38f495c2820617855a986449b3cde069
Ritik Harchani <harchani.ritik@gmail.com>
2019-05-16 06:48:58 +0530

    Add custom class for Adapter

---
+++ app/src/main/java/com/example/android/languagelearner/WordAdapter.java
@@ -0,0 +1,34 @@
+package com.example.android.languagelearner;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class WordAdapter extends ArrayAdapter<Word> {
+
+    public WordAdapter(Activity context, ArrayList<Word>words)
+    {
+        super(context,0,words);
+    }
+    @Override
+    public View getView(int position,View convertView,ViewGroup parent) {
+
+        Word words = getItem(position);
+
+        if(convertView==null){
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+        }
+
+        TextView sindhiTranslation=(TextView)convertView.findViewById(R.id.textViewA);
+        TextView dafaultTranslation=(TextView)convertView.findViewById(R.id.textViewB);
+
+        sindhiTranslation.setText(words.getSindhiTranslation());
+        dafaultTranslation.setText(words.getDefaultTranslation());
+        return convertView;
+    }
+}
