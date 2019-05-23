4ddbcfeacbec62f28cd66996ef88d9ce3b7bf052
Ritik Harchani <harchani.ritik@gmail.com>
2019-05-24 00:30:55 +0530

    Cleaning up Media Resources

--- app/src/main/java/com/example/android/languagelearner/Numbers.java
+++ app/src/main/java/com/example/android/languagelearner/Numbers.java
@@ -13,6 +13,24 @@ import java.util.ArrayList;
 public class Numbers extends AppCompatActivity {
 
     private MediaPlayer mMediaPlayer;
+    /**
+     * This listener gets triggered when the {@link MediaPlayer} has completed
+     * playing the audio file.
+     */
+    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            // Now that the sound file has finished playing, release the media player resources.
+            releaseMediaPlayer();
+        }
+    };
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mMediaPlayer.pause();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,19 +57,42 @@ public class Numbers extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
+                // Release the media player if it currently exists because we are about to
+                // play a different sound file
+                releaseMediaPlayer();
+
                 Word word = words.get(position);
                 mMediaPlayer = MediaPlayer.create(Numbers.this,word.getAudioResourceId());
                 mMediaPlayer.start();
 
-                /*
+                // Setup a listener on the media player, so that we can stop and release the
+                // media player once the sound has finished playing.
+                mMediaPlayer.setOnCompletionListener(mCompletionListener);
+
                 mMediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
                     @Override
                     public void onCompletion(MediaPlayer mp) {
-                        Toast.makeText(Numbers.this,"DONE",Toast.LENGTH_SHORT).show();
+
                     }
                 });
-                */
+
             }
         });
+    }
+    /**
+     * Clean up the media player by releasing its resources.
+     */
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mMediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mMediaPlayer.release();
+
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mMediaPlayer = null;
+        }
     }
 }
